8a438cf331c788c235ccde3b719b96b422f61377
Ata <ahakcil@gmail.com>
2020-12-15 14:45:32 +0300

    Function declerations for  text UI wrapper MarkovPasswords

--- Win-visualstudio/MarkovPasswords/src/main.cpp
+++ Win-visualstudio/MarkovPasswords/src/main.cpp
@@ -1,23 +1,37 @@
 
 #include <iostream>
 #include "color/term.h"
+#include "../../MarkovModel/src/model.h"
 #include <fstream>
 
 
+/* Initialize empty markov model
+*/
+Markov::Model* initialize();
+
+/* initialize model from exported model save file.
+*/
+Markov::Model* initialize(char* filename);
+
+/* open a dataset file and return ofstream ptr
+*/
 std::ofstream OpenDatasetFile(char* filename);
 
 
+/* Read dataset line by line and adjust model with each line
+*/
+void Train(std::ofstream);
+
+/* Save current model state to file
+*/
+void Save(char* filename)
 
 
-
-
-
-void dll_loadtest();   // a function from a DLL
 
 int main(int argc, char** argv) {
+
 	terminal t;
 	std::cout << TERM_SUCC << terminal::color::RED  <<  "Library loaded." << terminal::color::RESET << std::endl;
-	dll_loadtest();
 
 	return 0;
 }
