15157b826ebed6959b241cd5bb25dfe1945188d8
Ata <ahakcil@gmail.com>
2020-12-15 18:30:30 +0300

    Added example html render to the GUI

--- Win-visualstudio/MarkovPasswordsGUI/MarkovPasswordsGUI.cpp
+++ Win-visualstudio/MarkovPasswordsGUI/MarkovPasswordsGUI.cpp
@@ -1,7 +1,22 @@
 #include "MarkovPasswordsGUI.h"
+#include <fstream>
+#include <qwebengineview.h>
 
+#include <Windows.h>
 MarkovPasswordsGUI::MarkovPasswordsGUI(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    QWebEngineView* webkit = ui.centralWidget->findChild<QWebEngineView*>("chartArea");
+
+    //get working directory
+    char path[255];
+    GetCurrentDirectoryA(255, path);
+
+    //get absolute path to the layout html
+    std::string layout = "file:///" + std::string(path) + "\\views\\example.html";
+    std::replace(layout.begin(), layout.end(), '\\', '/');
+    webkit->setUrl(QUrl(layout.c_str()));
+
 }
