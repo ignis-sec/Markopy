9f2e5225eec0897fbff2602e3664bc3485107b78
Ata <ahakcil@gmail.com>
2021-01-08 06:26:38 +0300

    Pseudo-implement randomwalk function

--- Win-visualstudio/MarkovModel/src/model.cpp
+++ Win-visualstudio/MarkovModel/src/model.cpp
@@ -1,5 +1,6 @@
 #include "model.h"
 #include <fstream>
+#include <assert.h>
 
 /* Import model structure from a savefile
 */
@@ -13,11 +14,25 @@ bool Markov::Model::Import(char* filename) {
 
 /* Export model structure to a savefile
 */
-
 bool Markov::Model::Export(std::ofstream* f) {/*TODO*/}
 
 bool Markov::Model::Export(char* filename) {
 	std::ofstream exportfile;
 	exportfile.open(filename);
 	this->Export(&exportfile);
+}
+
+
+void Markov::Model::RandomWalk() {
+	Markov::Node* n = this->starterNode;
+	int len = 0;
+	char ret[32] = "";
+	while (n != NULL) {
+		n = n->RandomNext();
+		ret[len++] = n->value();
+		assert(len<32 && "return buffer overflowing, this will segfault if not aborted.");
+	}
+	ret[len] == '\0';
+
+	//do something with the generated string
 }
