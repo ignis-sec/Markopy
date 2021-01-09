21f046920e944c90407087ed6f807b331e1a35e6
Ata <ahakcil@gmail.com>
2021-01-10 00:27:34 +0300

    Added more documentation tools

--- documentation/mcss.conf.py
+++ documentation/mcss.conf.py
@@ -1,11 +1,16 @@
-
+import os
 HTML_EXTRA_FILES = "../README.md"
 
 DOXYFILE = 'Doxyfile-mcss'
 MAIN_PROJECT_URL = "https://github.com/FlameOfIgnis/CNG491-Project"
 
 SHOW_UNDOCUMENTED = True
+HTML_HEADER = open('documentation/includes/header.html').read()
 
+
+x = ["images/" + (item) for item in os.listdir('documentation/images/')]
+print(x)
+HTML_EXTRA_FILES = x
 
 FINE_PRINT = """CNG 491 - 2020  <br>
 Ata Hakçıl <br>
