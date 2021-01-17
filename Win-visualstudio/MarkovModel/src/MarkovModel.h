0660f0bbdf4b576923cfe9cc44e1adcd28be3154
Ata <ahakcil@gmail.com>
2021-01-17 16:46:43 +0300

    Easy way out of the template instantiation for now

---
+++ Win-visualstudio/MarkovModel/src/MarkovModel.h
@@ -0,0 +1,13 @@
+
+
+
+/* @brief
+* Templates are instantiated during compile time, 
+* so alternative to this approach is instantiating the classes/methods
+* with supported/allowed types to avoid a linker error
+*/
+
+#pragma once
+#include "model.cpp"
+#include "node.cpp"
+#include "edge.cpp"
