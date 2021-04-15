68fe490fc9d98fe45ca311aa04695b6162818e2b
Ata Hakçıl <ahakcil@gmail.com>
2021-04-15 16:35:49 +0300

    Add command line parameters descriptions

--- Win-visualstudio/MarkovPasswords/src/argparse.cpp
+++ Win-visualstudio/MarkovPasswords/src/argparse.cpp
@@ -5,8 +5,17 @@ ProgramOptions* Argparse::parse(int argc, char** argv) { return 0; }
 
 void Argparse::help() {
 	std::cout << 
-	"Markov Passwords - Help"
+	"Markov Passwords - Help\n"
 	"Options:\n"
+	"	\n"
+	"	-of --outputfilename\n" 
+	" 		Filename to output the generation results\n"
+	"	-ef --exportfilename\n"
+	" 		filename to export built model to\n"
+	"	-if --importfilename\n"
+	" 		filename to import model from\n"
+	"	-n (generate count)\n"
+	" 		Number of lines to generate\n"
 	"	\n"
 	
 	 << std::endl;
