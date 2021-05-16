c7315cb0916f59f87bdfad8ff91f1f75f2ccbe34
Ata Hakçıl <ahakcil@gmail.com>
2021-05-17 00:06:48 +0300

    Update build_minor.yml

---
+++ .github/workflows/action_helpers/actions_linux_setup_boost.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz -O /home/runner/boost_1_76_0.tar.gz -q;
+cd /home/runner/;
+tar -xf /home/runner/boost_1_76_0.tar.gz;
+cd /home/runner/boost_1_76_0/;
+./bootstrap.sh --with-python=$(which python$1) --with-python-version=$1;
+sudo ./b2 --with-python --with-program_options stage;
