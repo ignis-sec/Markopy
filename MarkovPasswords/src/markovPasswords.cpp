13550189aa3c0329b1f2bc0f31da5cdf36c87192
Ata Hakçıl <ahakcil@gmail.com>
2021-05-20 22:50:43 +0300

    Fix memory leak from training function

--- MarkovPasswords/src/markovPasswords.cpp
+++ MarkovPasswords/src/markovPasswords.cpp
@@ -51,6 +51,7 @@ void MarkovPasswords::Train(const char* datasetFileName, char delimiter)   {
 		sscanf(line.c_str(), format_str, &oc, linebuf);
 #endif
 		this->AdjustEdge((const char*)linebuf, oc); 
+		delete linebuf;
 	}
 	
 }
