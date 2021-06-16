58bc16483b9eee366c4e7b469c9e2e06d706cc9a
celalcetiner-01 <73341830+celalcetiner-01@users.noreply.github.com>
2021-06-16 20:04:19 +0300

    markopy.cpp edited

--- Markopy/src/Module/markopy.cpp
+++ Markopy/src/Module/markopy.cpp
@@ -4,6 +4,7 @@
 #define BOOST_PYTHON_STATIC_LIB
 #include <Python.h>
 #include <boost/python.hpp>
+#include <MarkovPasswords/src/modelMatrix.h>
 
 using namespace boost::python;
 
@@ -14,23 +15,23 @@ namespace Markov::Markopy{
         bool (Markov::API::MarkovPasswords::*Export)(const char*) = &Markov::Model<char>::Export;
         class_<Markov::API::MarkovPasswords>("MarkovPasswords", init<>())
             .def(init<>())
-            .def("Train", &Markov::API::MarkovPasswords::Train, 
-            "Train the model\n"
-            "\n"
-            ":param datasetFileName: Ifstream* to the dataset. If null, use class member\n"
-            ":param delimiter: a character, same as the delimiter in dataset content\n"
-            ":param threads: number of OS threads to spawn\n")
-            .def("Generate", &Markov::API::MarkovPasswords::Generate, 
-            "Generate passwords from a trained model.\n"
-            ":param n: Ifstream* to the dataset. If null, use class member\n"
-            ":param wordlistFileName: a character, same as the delimiter in dataset content\n"
-            ":param minLen: number of OS threads to spawn\n"
-            ":param maxLen: Ifstream* to the dataset. If null, use class member\n"
-            ":param threads: a character, same as the delimiter in dataset content\n"
-            ":param threads: number of OS threads to spawn\n")
+            .def("Train", &Markov::API::MarkovPasswords::Train)
+            .def("Generate", &Markov::API::MarkovPasswords::Generate)
             .def("Import", Import, "Import a model file.")
             .def("Export", Export, "Export a model to file.")
         ;
+        bool (Markov::API::ModelMatrix:: * Import1)(const char*) = &Markov::Model<char>::Import;
+        bool (Markov::API::ModelMatrix:: * Export1)(const char*) = &Markov::Model<char>::Export;
+        class_<Markov::API::ModelMatrix>("ModelMatrix", init<>())
+            
+            .def(init<>())
+            .def("Train", &Markov::API::ModelMatrix::Train)
+            .def("Import", Import1, "Import a model file.")
+            .def("Export", Export1, "Export a model to file.")
+            .def("ConstructMatrix",&Markov::API::ModelMatrix::ConstructMatrix)
+            .def("DumpJSON",&Markov::API::ModelMatrix::DumpJSON)
+            .def("FastRandomWalk",&Markov::API::ModelMatrix::FastRandomWalk)
+            ;
     };
 };
 
